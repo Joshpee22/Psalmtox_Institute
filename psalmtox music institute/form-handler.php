736e46bbc31f03e1aa4801d3e4d0b1152da3d106
joshpee22 <owobujoshua2017@gmail.com>
2024-05-31 15:59:52 -0700

    updated

--- psalmtox music institute/form-handler.php
+++ psalmtox music institute/form-handler.php
@@ -12,7 +12,7 @@
                     "Subject: $subject.\n".
                      "User Message: $message .\n";   
 
-$to = 'papunaberdzeni@gmail.com';
+$to = 'owobujoshua2017@gmail.com';
 $headers = "From: $email_from \r\n";
 $headers .="Reply-To: $visitor_email \r\n";
 
