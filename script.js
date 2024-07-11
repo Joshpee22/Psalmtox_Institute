14e273c9bbfaebaf6249c084cafa7d8e1283e888
joshpee22 <owobujoshua2017@gmail.com>
2024-07-11 19:44:13 +0100

    updated

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ function showMenu(){
 function hideMenu(){
     navLinks.style.top = '-800px'
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const images = document.querySelectorAll('.animated-img');
+    let currentIndex = 0;
+
+    function showNextImage() {
+        images[currentIndex].style.opacity = 0;
+        currentIndex = (currentIndex + 1) % images.length;
+        images[currentIndex].style.opacity = 1;
+    }
+
+    setInterval(showNextImage, 5000); // Change image every 3 seconds
+});
